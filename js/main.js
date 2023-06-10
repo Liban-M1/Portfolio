30295742201581f36799a4c8724262a26f739406
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-06-11 00:16:56 +0200

    .

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("fs-video");
+    video.style.display = "block"; 
+  
+    setTimeout(function() {
+      video.style.display = "none";
+    }, 4200);
+  });
+  
+  
+  
+  
+  
+  
+  
